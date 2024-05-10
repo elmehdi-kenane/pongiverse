50bdc43a24856d70bc0a54863f2ce0132594f185
Imad Dabligi <imad.dabligi@gmail.com>
2024-05-10 17:05:33 +0100

    feat(Dashboard): Creating Dashboard component/ Setupping head and body Dashboard

--- frontend/src/Dashboard/Dashboard.jsx
+++ frontend/src/Dashboard/Dashboard.jsx
@@ -1,50 +1,127 @@
 import React, { useState } from 'react'
-import { useEffect, useContext } from 'react';
-import { useNavigate, useLocation, json } from 'react-router-dom';
-import AuthContext from '../navbar-sidebar/Authcontext';
+import './Dashboard.css'
+import chart from "./chart.png"
+import statics from "./ss.png"
+import img from "../assets/SignUp/42_logo.svg"
+import img2 from "./Group2.svg"
+import img3 from "./Group3.svg"
+
 
 const Dashboard = () => {
-  const { user, notifSocket } = useContext(AuthContext)
-  const [users, setUsers] = useState([])
-  let a = 0
-  
-  useEffect(() => {
-    const getUsers = async () => {
-        const response = await fetch(`http://localhost:8000/users/profile/${user}`, {
-          method: 'GET'
-        })
-        const res = await response.json()
-        // set
-        console.log(res)
-        setUsers(res)
-        // document.write(res)
-    }
-    if (user)
-      getUsers()
-  }, [user])
 
-  const addFriend = async (myuser) => {
-    const response = await fetch(`http://localhost:8000/users/add/${user}`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        user : myuser,
-      })
-    })
-    const res = await response.json()
-  }
+  let i = 0;
+  const headElements = ["Matches : 15", "Wins: 9", "Loses: 6",
+    "Win ratio: 2.4"]
+  const rankData = [
+    {
+      name: "rennacir",
+      level: "15.4 XP"
+    },
+    {
+      name: "idabligi",
+      level: "13.4 XP"
+    },
+    {
+      name: "mmaqbour",
+      level: "12.7 XP"
+    },
+    {
+      name: "aagouzou",
+      level: "9.3 XP"
+    },
+    {
+      name: "aagouzou",
+      level: "9.3 XP"
+    },
+    {
+      name: "aagouzou",
+      level: "9.3 XP"
+    },
+    {
+      name: "aagouzou",
+      level: "9.3 XP"
+    },
+    {
+      name: "aagouzou",
+      level: "9.3 XP"
+    },
+    {
+      name: "aagouzou",
+      level: "9.3 XP"
+    },
+    {
+      name: "aagouzou",
+      level: "9.3 XP"
+    },
+    {
+      name: "aagouzou",
+      level: "9.3 XP"
+    },
+    {
+      name: "aagouzou",
+      level: "9.3 XP"
+    }
+  ]
+
+  const RankClassment = (position, player) => {
+    let trophyClass = "";
+    let nameClass = "";
+    if (position <= 3){
+      trophyClass = `rank--winners rank--${position}`;
+      nameClass = `rank__champion`;
+    }
+    
+  
+    return (
+      <>
+        <div className='rank__pic_trp'>
+          <p className={trophyClass}> #{position}</p>
+          <img src={img3} alt="Player" />
+        </div>
+        <div className="classment__level">
+          <p className={nameClass}> {player.name} </p>
+          <p className={nameClass}> {player.level} </p>
+        </div>
+      </>
+    );
+  };
 
   return (
-    <div style={{color:"white"}}>Dashboard
-      <ul>
-        {users.map((myuser) => (
-          <li onClick={() => {
-            addFriend(myuser)
-          }} key={a++}>{myuser}</li>
-        ))}
-      </ul>
+    <div className='dashpage'>
+      <div className="dashpage__head dash--space"> 
+        {headElements.map((item) => {
+          return (
+            <div className='dashpage__head__element dash--bkborder'> 
+              <p> {item} </p>
+              <img src={chart} />
+            </div>
+          )
+        })}
+      </div>
+      <div className="dashpage__body dash--space">
+        <div className="dashpage__body__statistics dash--bkborder">
+          <h1> Level Historics </h1>
+          <img src={statics} />
+        </div>
+        <div className="dashpage__body__rank dash--bkborder">
+          <h1> Rank </h1>
+          <div className='dashpage__body__rank__title'>
+            <div className='title'> Level </div>
+            <div className='title'> Win </div>
+            <div className='title'> Goal </div>
+          </div>
+          <div className="dashpage__body__rank__classment">
+            {rankData.map((player) => {
+              return (
+                <div className='classment'>
+                  {RankClassment(i = i + 1, player)}
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      </div>
+      <div className="dashpage__footer"> </div>
     </div>
   )
 }
