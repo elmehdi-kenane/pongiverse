bb1fed4cff73002aafe2ef5f436bdc12c6361591
Abdellah Agouzoul <aagouzou@e2r13p2.1337.ma>
2024-05-19 20:09:40 +0100

    add assets for chat and gourps pages in spreate file

---
+++ frontend/src/assets/chat/media/index.js
@@ -0,0 +1,8 @@
+export { default as ChannelsIcon } from './ChannelsIcon.svg';
+export { default as CreateChannel } from './CreateChannel.svg';
+export { default as DefaultAvatar } from './DefaultAvatar.svg';
+export { default as FriendsIcon } from './FriendsIcon.svg';
+export { default as InviteToPlay } from './InviteToPlay.svg';
+export { default as JoinChannel } from './JoinChannel.svg';
+export { default as SearchIcon } from './SearchIcon.svg';
+export { default as ThreePoints } from './ThreePoints.svg';
