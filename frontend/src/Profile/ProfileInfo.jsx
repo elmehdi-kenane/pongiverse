faba67fb4b12d4d1897424e264dc307932963227
Imad Dabligi <imad.dabligi@gmail.com>
2024-06-09 11:29:43 +0100

    refactor(Profile): UserFreind component almost done/ Working on loading

--- frontend/src/Profile/ProfileInfo.jsx
+++ frontend/src/Profile/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import {React, useState} from 'react'
 import EditIcon from '@mui/icons-material/Edit';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
+import HowToRegIcon from '@mui/icons-material/HowToReg';
 
 // import clan from "./assets/Frame.svg"
 // import clan4 from "./assets/Frame4.svg"
@@ -8,6 +9,50 @@ import clan5 from "./assets/Frame5.svg"
 import mavSvg from "./assets/Group.svg"
 import bg1 from "./assets/bg1.jpg"
 import bg2 from "./assets/bg2.jpg"
+
+
+
+  const UserFriend = () => {
+    const [isFriend, setIsFriend] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+
+    const handleFriendClick = () => {
+      setIsLoading(true);
+
+      setTimeout(() => {
+        console.log("Waited 3 Sec");  
+        setIsLoading(false);
+      }, 2500);
+
+      setIsFriend(!isFriend);
+    }
+
+    if (isLoading === true) {
+      return (
+        <div className="userinfo__is-friends no-select info-position friends-padding" >
+            <p> Waiting </p>
+        </div>
+      )
+    }
+    else if (isFriend === true ){
+      return (
+        <div className="userinfo__is-friends no-select info-position friends-padding" 
+        onClick={handleFriendClick}>
+            <HowToRegIcon />
+            <p> Friends </p>
+        </div>
+      )
+    }
+    else if (isFriend === false ){
+        return (
+          <div className="userinfo__is-friends no-select info-position" 
+          onClick={handleFriendClick}>
+            <PersonAddIcon />
+            <p> Add Friend </p>
+          </div> 
+        )
+    }
+  }
 
 function ProfileInfo() {
 
@@ -17,12 +62,7 @@ function ProfileInfo() {
   return (
     <div className="profile-userinfo purple-glass" style={{backgroundImage: `url(${bg1})`}}>
      
-      {isOwnProfile ? <EditIcon className='userinfo__edit info-position'/> :
-        <div className="userinfo__addfriend no-select info-position">
-          <PersonAddIcon />
-          <p> Add Friend </p>
-        </div>
-      }
+      {isOwnProfile ? <EditIcon className='userinfo__edit info-position'/> : <UserFriend />}
       
       <div className="userinfo__pic">
         <img src={mavSvg} alt="Player" />
