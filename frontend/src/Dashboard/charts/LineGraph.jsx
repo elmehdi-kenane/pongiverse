d242927cd9726878b2177f536aa276dfabcdbdbb
Imad Dabligi <imad.dabligi@gmail.com>
2024-07-02 14:34:52 +0100

    refactor(Dashboard): Edit ChatIcon Component Dimensions

--- frontend/src/Dashboard/charts/LineGraph.jsx
+++ frontend/src/Dashboard/charts/LineGraph.jsx
@@ -18,8 +18,8 @@ function LineGraph(props) {
         <AreaChart width={500} height={500} data={param.data} margin={{ left: param.left, right: param.right }}>
           <Area  stackId="1" type="mono-tone" dataKey={"wins"} stroke="#f4effa" fill="#f4effa" />
           <Area  stackId="1" type="mono-tone" dataKey={"lost"} stroke="#907ad6" fill="#826aed" />
-          <XAxis dataKey="day" stroke="#775C9E" />
-          <YAxis stroke="#775C9E" />
+          <XAxis dataKey="day" />
+          <YAxis />
           <CartesianGrid stroke="rgb(104, 104, 104, 0.3)" strokeDasharray="5 5" />
           <Tooltip content={<CustomToolTips/>} />
           <Legend wrapperStyle={{left: 0}} />
