e6503f1e575a3dbfc8a76083ecf840b7c3c534de
Imad Dabligi <imad.dabligi@gmail.com>
2024-07-02 12:03:22 +0100

    feat(Profile): Add BarIcon Component to Profile page

--- frontend/src/Profile/ProfileStatistics.jsx
+++ frontend/src/Profile/ProfileStatistics.jsx
@@ -1,24 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {Link} from "react-router-dom"
 
 import chatSvg from "../assets/navbar-sidebar/chat.svg"
 import { rankData } from '../Dashboard/helpers/rankData'
 import Pagination from "../Dashboard/helpers/Pagination"
 import BarGraph from "../Dashboard/charts/BarGraph"
+import LineGraph from '../Dashboard/charts/LineGraph'
 import { profileLevel } from './data/ProfileDataLevel';
 
-// import DashboardHead from '../Dashboard/DashboardHead'
-
+import BarChartIcon from '@mui/icons-material/BarChart';
+import ShowChartIcon from '@mui/icons-material/ShowChart';
 
 import { achv } from './data/achvData'
 
-
   function ProfileUserStatistics(){
+    const [isLineChart, setIsLineChart] = useState(false);
+    const iconClick = () => {
+      setIsLineChart(!isLineChart);
+    }
+
+    const chartParameters = {
+      left: -30,
+      right: 10,
+      data: profileLevel,
+      brSize: 15,
+    }
+
     return (
       <div className='userstate_statistics purple-glass-stats'>
         <div className='userstate-header'><h1> Statistics </h1> </div>
+        {isLineChart && <BarChartIcon className="statics__chart-icon" onClick={iconClick}/>}
+        {!isLineChart && <ShowChartIcon className="statics__chart-icon" onClick={iconClick}/>}
         <div className="statistics__container">
-          <BarGraph marginLeft={-35} marginRight={10} dataLevel={profileLevel} barSize={15}/>
+          {!isLineChart && <BarGraph param={chartParameters}/>}
+          {isLineChart && <LineGraph param={chartParameters}/>}
         </div>
       </div>
     )
