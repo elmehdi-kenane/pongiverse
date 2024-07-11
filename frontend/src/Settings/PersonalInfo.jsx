6ce7f4e57fe24ee47234798fd88f01955ad71f74
Imad Dabligi <imad.dabligi@gmail.com>
2024-07-11 18:02:31 +0100

    refactor(Settings): Handle Updating userInfo Component

--- frontend/src/Settings/PersonalInfo.jsx
+++ frontend/src/Settings/PersonalInfo.jsx
@@ -1,24 +1,71 @@
-import React from 'react'
+import React, { useRef, useState ,useEffect} from 'react'
 import mavSvg from './assets/Group.svg'
 import bg1 from "./assets/bg1.jpg"
 
-const userData = {
-  nickName : "Maverick",
-  bio : "Lorem ipsum dolor ...",
-  country : "Morocco",
-}
-
 const UpdateInfo = (props) => {
+  const [isUpdate, setIsUpdate] = useState(false);
+  const submit = !isUpdate ? "Update" : "Confirm";
+  const inputRef = useRef(null);
+  const iconRef = useRef(null);
+
+  useEffect(() =>{
+    if (inputRef.current)
+      inputRef.current.focus();
+  }, [isUpdate])
+  useEffect (()=> {
+  const handleOutsideClick = (event) => {
+    if ((iconRef.current && !event.composedPath().includes(iconRef.current))
+      && (inputRef.current && !event.composedPath().includes(inputRef.current))){
+    setIsUpdate(false);
+  }
+}
+const handleEsc = (event) => {
+  if (event.key === 'Escape') {
+      setIsUpdate(false);
+    }
+  };
+  document.body.addEventListener("click", handleOutsideClick)
+  document.body.addEventListener('keydown', handleEsc);
+  return () => {
+    document.body.removeEventListener("click", handleOutsideClick)
+    document.body.removeEventListener('keydown', handleEsc);
+  }
+},[])
+
+const onUpdate = () => {
+  if (isUpdate){
+    if (inputRef.current.value)
+      props.onEdit(inputRef.current.value)
+  }
+  setIsUpdate(!isUpdate);
+}
+const handleInputKeyDown = (event) => {
+  if (event.key === 'Enter'){
+    if (inputRef.current.value)
+      props.onEdit(inputRef.current.value)
+    setIsUpdate(false);
+    }
+  }
   return (
     <div className="update"> 
         <p className='title'> {props.title} </p>
-        <p className='info'> {props.value} </p>
-        <div className="upload-btn"> Update </div>
+        {!isUpdate && <p className='update__info'> {props.value} </p>}
+        {isUpdate && <input type="text" 
+                            className="update__input" 
+                            placeholder='Enter new Nickname... '
+                            onKeyDown={handleInputKeyDown}
+                            ref={inputRef}/>}
+        <div className="update__btn" onClick={onUpdate} ref={iconRef}> {submit} </div>
     </div>
   )
 }
 
 function PersonalInfo() {
+
+  const [nickName, setNickName] = useState("Maverick");
+  const [bio, setBio] = useState("Lorem ipsum dolor ...");
+  const [country, setCountry] = useState("Morocco");
+
   return (
     <div className="settings__personal-info ">
         <h1 className='personal-info__header'> PERSONAL INFO </h1>
@@ -26,16 +73,16 @@ function PersonalInfo() {
           <div className="update"> 
             <img  src={mavSvg} alt="UserPic"/>
             <p className='title-pic'> Upload a new picture </p>
-            <div className="upload-btn"> Update </div>
+            <div className="update__btn"> Update </div>
           </div>
           <div className="update"> 
             <img  src={bg1} alt="UserPic"/>
             <p className='title-pic'> Upload a new walppaper </p>
-            <div className="upload-btn"> Update </div>
+            <div className="update__btn"> Update </div>
           </div>
-          <UpdateInfo title="Nickname" value={userData.nickName}/>
-          <UpdateInfo title="Bio" value={userData.bio}/>
-          <UpdateInfo title="Country" value={userData.country}/>
+          <UpdateInfo title="Nickname" value={nickName} onEdit={setNickName}/>
+          <UpdateInfo title="Bio" value={bio} onEdit={setBio}/>
+          <UpdateInfo title="Country" value={country} onEdit={setCountry}/>
         </div>
     </div>
   )
