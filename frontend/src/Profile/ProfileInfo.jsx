0ab4efc366cbc6ac439e6a99aaa8a7c371725b1d
IMADDABLIGI <imad.dabligi@gmail.com>
2024-07-18 20:32:24 +0100

    refactor(Profile): Change ProfilePic State

--- frontend/src/Profile/ProfileInfo.jsx
+++ frontend/src/Profile/ProfileInfo.jsx
@@ -1,29 +1,38 @@
-import {React} from 'react'
+import {React, useContext, useEffect, useState} from 'react'
+import AuthContext from '../navbar-sidebar/Authcontext';
+
 import EditIcon from '@mui/icons-material/Edit';
 
 import IsFriends from './FriendOptions/IsFriends';
 import Report from './Report/Report';
 
+// import mavSvg from "./assets/Group.svg"
 // import clan from "./assets/Frame.svg"
 // import clan4 from "./assets/Frame4.svg"
 import clan5 from "./assets/Frame5.svg"
-import mavSvg from "./assets/Group.svg"
 import bg1 from "./assets/bg1.jpg"
 // import bg2 from "./assets/bg2.jpg"
 
 function ProfileInfo() {
+  
+  const {userPic} = useContext(AuthContext);
+  const {bio} = useContext(AuthContext);
 
-    const bio = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim."
-    // const [isOwnProfile, setIsOwnProfile] = useState(false);
+  const [localPic, setLocalPic] = useState(userPic);
+  
     const isOwnProfile = false;
-
+    // const [isOwnProfile, setIsOwnProfile] = useState(false);
+    // useEffect(() => {
+    //   setLocalPic(userPic)
+    //   console.log("change in userPic ..")
+    // }, [userPic])
   return (
     <div className="profile-userinfo purple-glass" style={{backgroundImage: `url(${bg1})`}}>
 
       {isOwnProfile ? <EditIcon className='userinfo__edit info-position'/> : <IsFriends />}
       
       <div className="userinfo__pic">
-        <img src={mavSvg} alt="Player" />
+        <img src={localPic} alt="Player" />
         <div className="is-online no-select"> Online </div>
       </div>
       <div className="userinfo__name-bio">
