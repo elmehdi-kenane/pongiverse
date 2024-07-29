b4e0e3b7ffe4f925f8f87e0ccb596007e2fd58ee
Rida Ennaciri <rennacir@e3r2p4.1337.ma>
2024-07-29 09:04:56 +0100

    feat(Tournament and auth):for merging

--- frontend/src/Dashboard/Dashboard.jsx
+++ frontend/src/Dashboard/Dashboard.jsx
@@ -1,21 +1,41 @@
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
 import './Dashboard.css'
 
 import DashboardHead from './DashboardHead';
 import DashboardBody from './DashboardBody';
 import DashboardFooter from './DashboardFooter';
+import AuthContext from '../navbar-sidebar/Authcontext';
 
 
 const Dashboard = () => {
+	const { user } = useContext(AuthContext)
+	const set_is_inside = async () => {
+		const response = await fetch(`http://localhost:8000/api/set-is-inside`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				user: user,
+				is_inside: false
+			})
+		})
+	}
 
-  return (
+	useEffect(() => {
+		if (user) {
+			set_is_inside()
+		}
+	}, [user])
 
-    <div className='dashpage'>
-      <DashboardHead />
-      <DashboardBody />
-      <DashboardFooter />
-    </div>
-  )
+	return (
+
+		<div className='dashpage'>
+			<DashboardHead />
+			<DashboardBody />
+			<DashboardFooter />
+		</div>
+	)
 }
 
 export default Dashboard
