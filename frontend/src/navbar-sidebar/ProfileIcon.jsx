2b82a6784dfff9793f5ab9786e131467a6f2c2f4
Imad Dabligi <imad.dabligi@gmail.com>
2024-08-08 19:16:31 +0100

    feat(NavBar): Set ProfileIcon Component user Pictures

--- frontend/src/navbar-sidebar/ProfileIcon.jsx
+++ frontend/src/navbar-sidebar/ProfileIcon.jsx
@@ -1,54 +1,59 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom';
+import AuthContext from './Authcontext';
 
 const ProfileIcon = ({ Icons, profileHandleDropDown, profileDropDownisOpen }) => {
-  return (
-    <div id="profile-icon" onClick={profileHandleDropDown}>
-        <a href="#">
-            <img src={Icons.profilepic} alt="" />
-        </a>
-        {profileDropDownisOpen && (<div className='profile-dropdown'>
+
+    const {user, userImg} = useContext(AuthContext);
+
+    return (
+        <div id="profile-icon" onClick={profileHandleDropDown}>
+            <a href="#">
+                <img src={userImg} alt="" />
+            </a>
+            {profileDropDownisOpen && (<div className='profile-dropdown'>
                 <div className='dropdown-profile-name'>
-                <div id='dropdown-profile-pic'>
-                    <a href='#'>
-                        <img src={Icons.profilepic} alt="profile-pic"/>
-                    </a>
+                    <div id='dropdown-profile-pic'>
+                        <Link to={`/mainpage/profile/${user}`}>
+                            <img src={userImg} alt="profile-pic" />
+                        </Link>
+                    </div>
+                    <div id='dropdown-profile-name'>
+                        <Link to={`/mainpage/profile/${user}`}>
+                            {user}
+                        </Link>
+                    </div>
                 </div>
-                <div id='dropdown-profile-name'>
-                    <a href='#'>
-                        Simo maqbour
-                    </a>
+                <div className='separator'>
+                    <div id='line-break'></div>
                 </div>
-            </div>
-            <div className='separator'>
-                <div id='line-break'></div>
-            </div>
-            <div className='settings-symbol-text'>
-                <div id='settings-symbol'>
-                    <a href="#">
-                        <img src={Icons.settings} alt="settings-logo" />
-                    </a>
+                <div className='settings-symbol-text'>
+                    <div id='settings-symbol'>
+                        <a href="#">
+                            <img src={Icons.settings} alt="settings-logo" />
+                        </a>
+                    </div>
+                    <div id='settings-text'>
+                        <Link to="/mainpage/settings">
+                            Settings
+                        </Link>
+                    </div>
                 </div>
-                <div id='settings-text'>
-                    <a href="#">
-                        Settings
-                    </a>
+                <div className='logout-symbol-text'>
+                    <div id='logout-symbol'>
+                        <a href="#">
+                            <img src={Icons.logout} alt="logout-logo" />
+                        </a>
+                    </div>
+                    <div id='logout-text'>
+                        <a href="#">
+                            Logout
+                        </a>
+                    </div>
                 </div>
-            </div>
-            <div className='logout-symbol-text'>
-                <div id='logout-symbol'>
-                    <a href="#">
-                        <img src={Icons.logout} alt="logout-logo" />
-                    </a>
-                </div>
-                <div id='logout-text'>
-                    <a href="#">
-                        Logout
-                    </a>
-                </div>
-            </div>
-        </div>)}
-    </div>
-  )
+            </div>)}
+        </div>
+    )
 }
 
 export default ProfileIcon;
