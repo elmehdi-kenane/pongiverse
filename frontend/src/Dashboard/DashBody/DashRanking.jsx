fa3dedd1f457b7fad6029a59104bab5b00312115
IMADDABLIGI <imad.dabligi@gmail.com>
2024-08-14 19:39:37 +0100

    feat(Dashboard): Dashboard Rank Done

--- frontend/src/Dashboard/DashBody/DashRanking.jsx
+++ frontend/src/Dashboard/DashBody/DashRanking.jsx
@@ -1,47 +1,125 @@
-import { React, useState } from 'react'
-import { rankData } from "../helpers/Data"
+import { React, useContext, useEffect, useState } from "react";
+import { rankData } from "../helpers/Data";
+import MavSvg from "../../assets/Profile/Group.svg"
 
-const RankClassment = (position, player) => {
+import AuthContext from "../../navbar-sidebar/Authcontext";
+
+function DashRanking() {
+  const { user } = useContext(AuthContext);
+  const [usersData, setUsersData] = useState([]);
+  const [userImages, setUserImages] = useState([]);
+  
+  const [sortOption, setSortOption] = useState('level');
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const promises = usersData.map(async (user) => {
+        const response = await fetch(`http://localhost:8000/profile/getUserImage/${user.username}`, {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+        const blob = await response.blob();
+        return URL.createObjectURL(blob);
+      });
+      const images = await Promise.all(promises);
+      setUserImages(images);
+    };
+    if (usersData.length)
+      fetchImages();
+  }, [usersData]);
+
+  useEffect(() => {
+    const getUsersData = async () => {
+      try {
+        const response = await fetch(
+          `http://localhost:8000/profile/getUsersData/${user}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const res = await response.json();
+        if (response.ok) 
+          setUsersData(res.usersData);
+        else
+          console.log("Error : ", res.error);
+      } catch (error) {
+        console.log("Error: ", error);
+      }
+    };
+    if (user)
+      getUsersData();
+  }, [user]);
+
+  const RankClassment = (position, player) => {
     let trophyClass = ""; // pos-pic
     let championsClass = ""; // name-level
     if (position <= 3) {
       trophyClass = `winners winner--${position}`;
       championsClass = `player__name_level--champions`;
     }
+    let userValue = player[sortOption];
     return (
       <>
-        <div className='player__pos_pic'>
+        <div className="player__pos_pic">
           <p className={trophyClass}> #{position}</p>
-          <img src={player.img} alt="Player" />
+          <img src={userImages.length ? userImages[position - 1] : MavSvg} alt="Player" />
         </div>
         <div className="player__name_level">
-          <p className={championsClass}> {player.name} </p>
-          <p className={championsClass}> {player.level} </p>
+          <p className={championsClass}> {player.username} </p>
+          <p className={championsClass}> {userValue} </p>
         </div>
       </>
     );
   };
 
-function DashRanking() {
-    return (
-        <div className="dashpage__body__rank dash--bkborder">
-            <h1> Rank </h1>
-            <div className='dashpage__body__rank__title'>
-                <div className='title'> Level </div>
-                <div className='title'> Win </div>
-                <div className='title'> Goal </div>
+  const sortUsersData = (option) => {
+    if (option === 'level'){
+      const sortByLevelAndXp = (a, b) => {
+        if (b.level === a.level)
+          return b.xp - a.xp;
+        return b.level - a.level;
+      };
+      usersData.sort(sortByLevelAndXp);
+    }
+    else if (option === 'goals'){
+      const sortByGoals = (a, b) => b.goals - a.goals;
+      usersData.sort(sortByGoals);
+    }
+    else if (option === 'wins'){
+      const sortByWins = (a, b) => b.wins - a.wins;
+      usersData.sort(sortByWins);
+    }
+  }
+
+  useEffect(()=>{
+    sortUsersData(sortOption);
+  },[sortOption])
+
+  return (
+    <div className="dashpage__body__rank dash--bkborder">
+      <h1> Rank </h1>
+      <div className="dashpage__body__rank__title">
+        <div className={`title ${sortOption === 'level' ? 'select' : ''}`} onClick={() => setSortOption('level')}> Level </div>
+        <div className={`title ${sortOption === 'wins' ? 'select' : ''}`}  onClick={() => setSortOption('wins')}> Wins </div>
+        <div className={`title ${sortOption === 'goals' ? 'select' : ''}`} onClick={() => setSortOption('goals')}> Goals </div>
+      </div>
+      <div className="dashpage__body__rank__classment">
+        {sortUsersData(sortOption)}
+        {usersData.map((player, key) => {
+          return (
+            <div className="player__classment" key={key}>
+              {RankClassment(key + 1, player)}
             </div>
-            <div className="dashpage__body__rank__classment">
-                {rankData.map((player, key) => {
-                    return (
-                        <div className='player__classment' key={key}>
-                            {RankClassment(key+1, player)}
-                        </div>
-                    )
-                })}
-            </div>
-        </div>
-    )
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
-export default DashRanking
+export default DashRanking;
