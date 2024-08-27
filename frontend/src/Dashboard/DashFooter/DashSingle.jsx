dfe5f4dc2b81f1a1384eef2b6deaeb6f5c6c8715
IMADDABLIGI <imad.dabligi@gmail.com>
2024-08-27 20:15:41 +0100

    feat(Dashboard): SingleMatches With Pagination Done

--- frontend/src/Dashboard/DashFooter/DashSingle.jsx
+++ frontend/src/Dashboard/DashFooter/DashSingle.jsx
@@ -1,19 +1,24 @@
 import React, { useContext, useEffect, useState } from "react";
-import idabligiSvg from "../assets/Group2.svg";
-import AgouzouSvg from "../assets/Group3.svg";
-import Pagination from "../helpers/Pagination";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
 
 import AuthContext from "../../navbar-sidebar/Authcontext";
+import { Link } from "react-router-dom";
 
 function DashSingle() {
   const { user } = useContext(AuthContext);
+
+  const [page, setPage] = useState(1)
+  const [index, setIndex] = useState(1)
+  const [limit, setLimit] = useState(-1)
   const [matches, setMatches] = useState([])
 
   useEffect(() => {
     const getSingleMatches = async () => {
       try {
         const response = await fetch(
-          `http://localhost:8000/profile/getSingleMatches/${user}`,
+          `http://localhost:8000/profile/getSingleMatches/${user}/${page}`,
           {
             method: "GET",
             headers: {
@@ -22,37 +27,55 @@ function DashSingle() {
           }
         );
         const res = await response.json();
-        if (response.ok)
-            setMatches(res.userMatches);
+        if (response.ok){
+          setMatches([...matches, ...res.userMatches]);
+          !res.hasMoreMatches && setLimit(index)
+        }
         else
             console.log("Error : ", res.error);
       } catch (error) {
         console.log("Error: ", error);
       }
     };
-    // if (user)
-    //     getSingleMatches();
-  }, [user]);
+    if (user)
+        getSingleMatches();
+  }, [user, page]);
 
+  const ExpandMore = () => {
+    setIndex(index+1);
+    index + 1 > page && setPage(page+1)
+    console.log("Index is:", index+1)
+  }
+  const ExpandLess = () => {
+    index-1 && setIndex(index-1);
+    console.log(index);
+  }
+
+  const itemsPerPage = 3;
   return (
     <div className="footer__single-match dash--bkborder">
       <h1 className="footer__titles"> Single Match </h1>
-      <div className="single-match__result footer__result">
-        <img src={idabligiSvg} alt="Player" />
-        <p> 5 - 3 </p>
-        <img src={AgouzouSvg} alt="Player" />
+      {matches.length ? matches
+      .slice((index - 1) * itemsPerPage, (index) * itemsPerPage)
+      .map((match, key) => (
+        <div key={key} className="single-match__result footer__result">
+          <img src={match.pic1} alt="Player" />
+          <p>{match.score}</p>
+          <img src={match.pic2} alt="Player" />
+        </div>
+      )): <div className="no-result"> 
+            <h3>You haven't participate in a Single Match before!</h3>
+            <Link to="/mainpage/game/solo/1vs1" className="start-game"> 
+              <p> Play </p>
+              <SportsEsportsIcon />
+             </Link>
+          </div> }
+      <div className="expand">
+        {index != 1 &&
+          <ExpandLessIcon className="expand-less" onClick={ExpandLess}/>}
+        {limit != index &&
+          <ExpandMoreIcon className="expand-more" onClick={ExpandMore}/>}
       </div>
-      <div className="single-match__result footer__result">
-        <img src={idabligiSvg} alt="Player" />
-        <p> 5 - 3 </p>
-        <img src={AgouzouSvg} alt="Player" />
-      </div>
-      <div className="single-match__result footer__result">
-        <img src={idabligiSvg} alt="Player" />
-        <p> 5 - 3 </p>
-        <img src={AgouzouSvg} alt="Player" />
-      </div>
-      <Pagination />
     </div>
   );
 }
--- frontend/src/Dashboard/helpers/Pagination.jsx
+++ frontend/src/Dashboard/helpers/Pagination.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ExpandLessIcon from '@mui/icons-material/ExpandLess';
-import '../Dashboard.css'
+import React from "react";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import "../Dashboard.css";
 
 function Pagination() {
-    return (
-        <div className="expand">
-          <ExpandLessIcon className='expand-less'/>
-          <ExpandMoreIcon className='expand-more'/>
-      </div>
-    )
+  return (
+    <div className="expand">
+      <ExpandLessIcon className="expand-less" />
+      <ExpandMoreIcon className="expand-more" />
+    </div>
+  );
 }
 
-export default Pagination
+export default Pagination;
