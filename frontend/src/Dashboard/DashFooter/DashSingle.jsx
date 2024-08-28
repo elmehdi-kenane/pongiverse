5941f3c21de079ae944b407de220a3cdc7d1c862
IMADDABLIGI <imad.dabligi@gmail.com>
2024-08-28 12:11:08 +0100

    feat(Dashboard): SingleMatches With Pagination Done2

--- frontend/src/Dashboard/DashFooter/DashSingle.jsx
+++ frontend/src/Dashboard/DashFooter/DashSingle.jsx
@@ -1,7 +1,8 @@
 import React, { useContext, useEffect, useState } from "react";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ExpandLessIcon from "@mui/icons-material/ExpandLess";
-import SportsEsportsIcon from '@mui/icons-material/SportsEsports';
+import SportsEsportsIcon from "@mui/icons-material/SportsEsports";
+import CircularProgress from '@mui/material/CircularProgress';
 
 import AuthContext from "../../navbar-sidebar/Authcontext";
 import { Link } from "react-router-dom";
@@ -9,13 +10,15 @@ import { Link } from "react-router-dom";
 function DashSingle() {
   const { user } = useContext(AuthContext);
 
-  const [page, setPage] = useState(1)
-  const [index, setIndex] = useState(1)
-  const [limit, setLimit] = useState(-1)
-  const [matches, setMatches] = useState([])
+  const [page, setPage] = useState(1);
+  const [index, setIndex] = useState(1);
+  const [loading, setLoading] = useState(false);
+  const [limit, setLimit] = useState(-1);
+  const [matches, setMatches] = useState([]);
 
   useEffect(() => {
     const getSingleMatches = async () => {
+      setLoading(true)
       try {
         const response = await fetch(
           `http://localhost:8000/profile/getSingleMatches/${user}/${page}`,
@@ -27,55 +30,63 @@ function DashSingle() {
           }
         );
         const res = await response.json();
-        if (response.ok){
+        if (response.ok) {
           setMatches([...matches, ...res.userMatches]);
-          !res.hasMoreMatches && setLimit(index)
-        }
-        else
-            console.log("Error : ", res.error);
+          !res.hasMoreMatches && setLimit(index);
+        } else console.log("Error : ", res.error);
       } catch (error) {
         console.log("Error: ", error);
       }
+      setLoading(false)
     };
-    if (user)
-        getSingleMatches();
+    if (user) getSingleMatches();
   }, [user, page]);
 
   const ExpandMore = () => {
-    setIndex(index+1);
-    index + 1 > page && setPage(page+1)
-    console.log("Index is:", index+1)
-  }
+    setIndex(index + 1);
+    index + 1 > page && setPage(page + 1);
+  };
   const ExpandLess = () => {
-    index-1 && setIndex(index-1);
-    console.log(index);
-  }
+    index - 1 && setIndex(index - 1);
+  };
 
   const itemsPerPage = 3;
   return (
     <div className="footer__single-match dash--bkborder">
       <h1 className="footer__titles"> Single Match </h1>
-      {matches.length ? matches
-      .slice((index - 1) * itemsPerPage, (index) * itemsPerPage)
-      .map((match, key) => (
-        <div key={key} className="single-match__result footer__result">
-          <img src={match.pic1} alt="Player" />
-          <p>{match.score}</p>
-          <img src={match.pic2} alt="Player" />
+      {loading ? 
+      <CircularProgress color="secondary" style={{marginTop:"80px"}}/>
+      :
+      <>
+      {matches.length ? 
+        <>
+        {matches.slice((index - 1) * itemsPerPage, index * itemsPerPage)
+          .map((match, key) => (
+            <div key={key} className="single-match__result footer__result">
+              <img src={match.pic1} alt="Player" />
+              <p>{match.score}</p>
+              <img src={match.pic2} alt="Player" />
+            </div>
+          ))
+        }
+        <div className="expand">
+          {index != 1 && (
+            <ExpandLessIcon className="expand-less" onClick={ExpandLess} />)}
+          {limit != index && (
+            <ExpandMoreIcon className="expand-more" onClick={ExpandMore} />)}
         </div>
-      )): <div className="no-result"> 
-            <h3>You haven't participate in a Single Match before!</h3>
-            <Link to="/mainpage/game/solo/1vs1" className="start-game"> 
-              <p> Play </p>
-              <SportsEsportsIcon />
-             </Link>
-          </div> }
-      <div className="expand">
-        {index != 1 &&
-          <ExpandLessIcon className="expand-less" onClick={ExpandLess}/>}
-        {limit != index &&
-          <ExpandMoreIcon className="expand-more" onClick={ExpandMore}/>}
-      </div>
+      </>
+      : 
+        <div className="no-result">
+          <h3>You haven't participated in a single match before!</h3>
+          <Link to="/mainpage/game/solo/1vs1" className="start-game">
+            <p> Play </p>
+            <SportsEsportsIcon />
+          </Link>
+        </div>
+      }
+      </>
+    }
     </div>
   );
 }
