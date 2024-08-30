9f7344c5b9534cc4c7198a682040c4c3fc205f92
El Mehdi Kenane <elmehdi.kenane@gmail.com>
2024-08-30 01:59:46 +0100

    added roomSerializer

--- backend/ft_trans/src/navBar/serializers.py
+++ backend/ft_trans/src/navBar/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from myapp.models import customuser
+from chat.models import Room
 
 class customUserSerializer(serializers.ModelSerializer):
     username = serializers.CharField()
@@ -11,7 +12,6 @@ class customUserSerializer(serializers.ModelSerializer):
         fields = ['username', 'is_friend','avatar']
     
     def get_is_friend(self, obj):
-        # Ensure that `is_friend` exists on the object
         return getattr(obj, 'is_friend', False)
     
     def get_avatar(self, obj):
@@ -22,4 +22,28 @@ class customUserSerializer(serializers.ModelSerializer):
                 return request.build_absolute_uri(avatar_url)
             else:
                 return f"http://localhost:8000/auth{avatar_url}"
+        return None
+
+class roomSerializer(serializers.ModelSerializer):
+    name = serializers.CharField()
+    is_joined = serializers.SerializerMethodField()
+    icon = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Room
+        fields = ['name', 'is_joined', 'icon']
+    
+    def get_is_joined(self, obj):
+        return getattr(obj, 'is_joined', False)
+
+    def get_icon(self, obj):
+        request = self.context.get('request')
+        if obj.icon:
+            icon_url = obj.icon.url
+            if request:
+                print(request.build_absolute_uri(icon_url))
+                return request.build_absolute_uri(icon_url)
+            else:
+                print(f"http://localhost:8000/auth{icon_url}")
+                return f"http://localhost:8000/auth{icon_url}"
         return None
