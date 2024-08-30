1bb242f4b1364c575b4c8d401ab79787298cf0c9
El Mehdi Kenane <elmehdi.kenane@gmail.com>
2024-08-30 02:01:35 +0100

    added handleAddFriendReq

--- frontend/src/Friends/SuggestionFriendCard.jsx
+++ frontend/src/Friends/SuggestionFriendCard.jsx
@@ -1,29 +1,11 @@
 import { useState, useContext } from 'react'
 import AuthContext from '../navbar-sidebar/Authcontext'
+import { handleAddFriendReq } from './utils'
 
 const SuggestionFriendCard = ({ secondUsername, avatar }) => {
     const { user } = useContext(AuthContext);
     const [friendRequestBtn, setFriendRequestBtn] = useState(false);
-    const handleAddFriendReq = () => {
-    fetch(`http://${import.meta.env.VITE_IPADDRESS}:8000/friends/add_friend_request/`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-            from_username: user,
-            to_username: secondUsername,
-        }),
-    })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-        setFriendRequestBtn(true);
-    };
+
     return (
         <div className="SuggestionFriendCard embla__slide">
             <div className="ProfileName">
@@ -39,7 +21,7 @@ const SuggestionFriendCard = ({ secondUsername, avatar }) => {
                     </div>
                 </>
                     :
-                    <button className="FriendBtn Add" onClick={handleAddFriendReq}>Add friend</button>
+                    <button className="FriendBtn Add" onClick={() => handleAddFriendReq(user, secondUsername, setFriendRequestBtn)}>Add friend</button>
                 }
         </div>
     )
--- frontend/src/Friends/utils.jsx
+++ frontend/src/Friends/utils.jsx
@@ -42,3 +42,25 @@ export const confirmFriendRequest = (currentUsername, secondUsername) => {
       console.error("Error:", error);
     });
 };
+
+
+export const handleAddFriendReq = (currentUsername, secondUsername, setFriendRequestBtn) => {
+    fetch(`http://${import.meta.env.VITE_IPADDRESS}:8000/friends/add_friend_request/`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            from_username: currentUsername,
+            to_username: secondUsername,
+        }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    setFriendRequestBtn(true);
+};
