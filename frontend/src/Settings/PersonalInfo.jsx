3b3e02d805cc8eeee79ef99ca413e8138aee189c
Imad Dabligi <imad.dabligi@gmail.com>
2024-10-10 21:24:03 +0100

    refactor(Settings): deleting AdjustPic component/ replacing it with UpdateAvatar and UpdateBg

--- frontend/src/Settings/PersonalInfo.jsx
+++ frontend/src/Settings/PersonalInfo.jsx
@@ -1,18 +1,42 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from 'react'
 import { Toaster } from "react-hot-toast";
 import SettingsContext from "./SettingsWrapper";
 import SettingsLeft from "./SettingsLeft";
 import "./Settings.css";
+import EditIcon from '@mui/icons-material/Edit';
 
 import UpdateNkName from './Update/UpdateNkName';
 import UpdateBio from './Update/UpdateBio';
 import UpdateCountry from './Update/UpdateCountry';
-import UpdatePic from './Update/UpdatePic';
-import AdjustPic from './Update/AdjustPic';
+import UpdateBg from './Update/UpdateBg';
+import UpdateAvatar from './Update/UpdateAvatar';
+
 
 function PersonalInfo() {
+  
+  const [isUpdate, setIsUpdate] = useState(false);
+  const { userPic, userBg } = useContext(SettingsContext)
 
-  const [isAdjustPic, setIsAdjustPic] = useState(false);
+  const UpdatePictures = () => {
+    return (
+      <>
+        <div className="update">
+          <img src={userPic} alt="userImg" />
+          <p className='title-pic'> Upload a new picture </p>
+          <div className="update__btn" onClick={() => setIsUpdate('avatar')}> <p> Update </p>
+            <EditIcon /> 
+          </div>
+        </div>
+        <div className="update">
+          <img src={userBg} alt="userBg" />
+          <p className='title-pic'> Upload a new wallpaper </p>
+          <div className="update__btn" onClick={() => setIsUpdate('background')}> <p> Update </p>
+            <EditIcon />
+          </div>
+        </div>
+      </>
+    )
+  }
 
   return (
     <div className="settings-page">
@@ -21,14 +45,16 @@ function PersonalInfo() {
       <div className="settings__personal-info ">
         <h1 className='settings__header'> PERSONAL INFO </h1>
         <div className="personal-info__update linear-purple-bg">
-          {isAdjustPic ? <AdjustPic setAdjust={setIsAdjustPic}/> :
+          {!isUpdate && 
             <>
-              <UpdatePic setAdjust={setIsAdjustPic}/>
+              <UpdatePictures />
               <UpdateNkName />
               <UpdateBio />
               <UpdateCountry />
             </>
           }
+          {isUpdate === 'avatar' && <UpdateAvatar setAdjust={setIsUpdate}/>}
+          {isUpdate === 'background' && <UpdateBg setAdjust={setIsUpdate}/>}
         </div>
       </div>
     </div>
