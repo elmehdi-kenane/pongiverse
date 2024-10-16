be361aaa07229329565c8d70f757736f190fa147
Imad Dabligi <imad.dabligi@gmail.com>
2024-10-16 19:48:13 +0100

    refactor(Profile): Move History Match to ProfileUserGames1vs1 Component/ Split to 1vs1 and 2vs2 Match History

--- frontend/src/Profile/Statistics/ProfileUserGames.jsx
+++ frontend/src/Profile/Statistics/ProfileUserGames.jsx
@@ -1,10 +1,5 @@
-import React, { useContext, useEffect, useState } from 'react'
-import mavSvg from "../../assets/Profile/Group.svg"
-import ProfileContext from "../ProfileWrapper"
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import ExpandLessIcon from "@mui/icons-material/ExpandLess";
-import CircularProgress from '@mui/material/CircularProgress';
-import { useNavigate } from 'react-router-dom';
+import React from 'react'
+import ProfileUserGames1vs1 from './ProfileUserGames1vs1';
 
 const MatchHeaderStates = () => {
   const matchHeader = ["Matches", "Date", "Score", "Hit", "Accuracy", "Rating"]
@@ -22,107 +17,13 @@ const MatchHeaderStates = () => {
 }
 
 function ProfileUserGames() {
-  const {userId} = useContext(ProfileContext);
-  const [userGames, setUserGames] = useState([])
-  const [page, setPage] = useState(1);
-  const [index, setIndex] = useState(1);
-  const [limit, setLimit] = useState(-1);
-  const itemsPerPage = 5;
-  const [loading, setLoading] = useState(false);
-
-  const getUserMatches = async (page, index) => {
-    setLoading(true)
-    try {
-      const response = await fetch(`http://localhost:8000/profile/getUserMatches1vs1/${userId}/${page}`, {
-        method: "GET",
-        headers: {
-          'Content-Type': 'application/json',
-        }
-      });
-      const res = await response.json()
-      if (response.ok){
-        (page === 1) ? setUserGames(res.data) : setUserGames([...userGames, ...res.data])
-        !res.hasMoreMatches && setLimit(index);
-        console.log("Data :", res.data);
-      }
-      else 
-      console.log("Error : ", res.error);
-    } catch (error) {
-      console.log("Error: ", error);
-    }      
-    setLoading(false)
-  }
-  
-  useEffect(()=> {
-    if (userId){
-      setPage(1);
-      setIndex(1);
-      setLimit(-1);
-      getUserMatches(1, 1)
-    }
-  }, [userId])
-
-  const Pagination = () => {
-    const expandMore = () => {
-      setIndex(index + 1);
-      if (index + 1 > page){
-        getUserMatches(page + 1, index + 1)
-        setPage(page + 1);
-      }
-    };
-    const expandLess = () => {
-      index - 1 && setIndex(index - 1);
-    };
-    return (
-      <div className="expand">
-        {index != 1 && (
-          <ExpandLessIcon onClick={expandLess} />)}
-        {limit != index && (
-          <ExpandMoreIcon onClick={expandMore} />)}
-      </div>
-    )
-  }
-
-  const MatchStates = () => {
-    const navigate = useNavigate();
-    return (
-      <>
-      {loading ? 
-        <CircularProgress color="secondary" style={{marginTop:"120px"}}/>
-        :
-        <>
-          {userGames.slice((index-1) * itemsPerPage, index * itemsPerPage)
-            .map((match, key)=> {
-              return (
-                <div className="match__states" key={key}>
-                  <div className='match__dtl match--players'> 
-                    <img src={match.pic1} alt="playerImg" onClick={()=>navigate(`/mainpage/profile/${match.user1}`)}/>
-                    <img src={match.pic2} alt="playerImg" onClick={()=>navigate(`/mainpage/profile/${match.user2}`)}/>
-                  </div>
-                  <div className='match__dtl match--date'> 
-                    <p> {match.time} </p>
-                    <p> {match.date} </p>
-                  </div>
-                  <p className='match__dtl'> {match.score} </p>
-                  <p className='match__dtl'> {match.hit1} - {match.hit2} </p>
-                  <p className='match__dtl'> {match.acc1} - {match.acc2} </p>
-                  <p className='match__dtl'> {match.exp1} - {match.exp2} </p>
-                </div>
-              )
-            })}
-          </>
-        }
-        <Pagination />
-      </>
-    )
-  }
 
   return (
     <div className='userstate_matches purple--glass'>
-        <div className='userstate-header'><h1> Match History </h1> </div>
-        <MatchHeaderStates />
-        <MatchStates />
-      </div>
+      <div className='userstate-header'><h1> Match History </h1> </div>
+      <MatchHeaderStates />
+      <ProfileUserGames1vs1 />
+    </div>
   )
 }
 
