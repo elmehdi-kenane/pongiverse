c223a26824600d00aae81d77c5982f80746622b6
a-agouzou <aagouzou@student.1337.ma>
2025-01-03 00:55:08 +0100

    monitoring-system docker

--- monitoring_system/setup.sh
+++ monitoring_system/setup.sh
@@ -2,16 +2,16 @@
 rm -rf alertmanager alertmanager.tar.gz data prometheus prometheus.tar.gz
 if lsof -ti :9090 > /dev/null; then
   echo "Port 9090 is in use. Killing the process..."
-  kill -9 $(lsof -ti :9090)
+#   kill -9 $(lsof -ti :9090)
 fi
 if lsof -ti :9094 > /dev/null; then
   echo "Port 9094 is in use. Killing the process..."
-  kill -9 $(lsof -ti :9094)
+#   kill -9 $(lsof -ti :9094)
 fi
 
 if lsof -ti :9100 > /dev/null; then
   echo "Port 9100 is in use. Killing the process..."
-  kill -9 $(lsof -ti :9100)
+#   kill -9 $(lsof -ti :9100)
 fi
 
 if [ ! -d "./archive" ]; then
@@ -21,22 +21,22 @@ if [ ! -d "./tools" ]; then
     mkdir tools
 fi
 
-echo -e "\033[1;34m ============ Brew 🌐 ============\033[0m"
-if [ -d "/goinfre/$USER/.brew" ]; then
-    echo -e "\033[1;34m ============ Brew already downloaded 💯 ============\033[0m"
-else
-    echo -e "\033[1;34m ============ Brew downloading ⏳... ============\033[0m"
-    bash /Users/$USER/brew/install.sh
-    echo -e "\033[1;34m ============ Brew downloaded 🔰 ============\033[0m"
-fi
-echo -e ""
+# echo -e "\033[1;34m ============ Brew 🌐 ============\033[0m"
+# if [ -d "/goinfre/$USER/.brew" ]; then
+#     echo -e "\033[1;34m ============ Brew already downloaded 💯 ============\033[0m"
+# else
+#     echo -e "\033[1;34m ============ Brew downloading ⏳... ============\033[0m"
+#     bash /Users/$USER/brew/install.sh
+#     echo -e "\033[1;34m ============ Brew downloaded 🔰 ============\033[0m"
+# fi
+# echo -e ""
 
 echo -e "\033[1;34m ============ Wget 🌐 ============\033[0m"
 if command -v wget >/dev/null 2>&1; then
     echo -e "\033[1;34m ============ Wget already downloaded 💯 ============\033[0m"
 else
     echo -e "\033[1;34m ============ Wget downloading ⏳... ============\033[0m"
-    brew install wget
+    apt update && apt install -y wget
     echo -e "\033[1;34m ============ Wget downloaded 🔰 ============\033[0m"
 fi
 echo -e ""
@@ -50,13 +50,13 @@ if [ -d "./tools/prometheus" ]; then
     echo -e "\033[1;34m ============ Prometheus already downloaded 💯 ============\033[0m"
 else
     echo -e "\033[1;34m ============ Prometheus downloading ⏳... ============\033[0m"
-    wget -q https://github.com/prometheus/prometheus/releases/download/v2.55.1/prometheus-2.55.1.darwin-amd64.tar.gz
+    wget -q https://github.com/prometheus/prometheus/releases/download/v2.55.1/prometheus-2.55.1.linux-amd64.tar.gz
     echo -e "\033[1;34m ============ Prometheus downloaded 🔰 ============\033[0m"
-    mv prometheus-2.55.1.darwin-amd64.tar.gz prometheus.tar.gz
+    mv prometheus-2.55.1.linux-amd64.tar.gz prometheus.tar.gz
     tar xfz prometheus.tar.gz
 
     mv prometheus.tar.gz archive
-    mv prometheus-2.55.1.darwin-amd64 tools/prometheus
+    mv prometheus-2.55.1.linux-amd64 tools/prometheus
 fi
 echo -e "\033[1;34m ============ Prometheus started ✅ at => http://localhost:9090 ============\033[0m"
 ./tools/prometheus/prometheus --config.file="config/prometheus/prometheus.yml" --storage.tsdb.path="./tools/prometheus/data" > logs/prometheus_logs 2>&1 &
@@ -70,13 +70,13 @@ if [ -d "./tools/alertmanager" ]; then
     echo -e "\033[1;34m ============ Alertmanager already downloaded 💯 ============\033[0m"
 else
     echo -e "\033[1;34m ============ Alertmanager downloading ⏳... ============\033[0m"
-    wget -q https://github.com/prometheus/alertmanager/releases/download/v0.28.0-rc.0/alertmanager-0.28.0-rc.0.darwin-amd64.tar.gz
+    wget -q https://github.com/prometheus/alertmanager/releases/download/v0.28.0-rc.0/alertmanager-0.28.0-rc.0.linux-amd64.tar.gz
     echo -e "\033[1;34m ============ Alertmanager downloaded 🔰 ============\033[0m"
-    mv alertmanager-0.28.0-rc.0.darwin-amd64.tar.gz alertmanager.tar.gz
+    mv alertmanager-0.28.0-rc.0.linux-amd64.tar.gz alertmanager.tar.gz
     tar xfz alertmanager.tar.gz
 
     mv alertmanager.tar.gz archive
-    mv alertmanager-0.28.0-rc.0.darwin-amd64 tools/alertmanager
+    mv alertmanager-0.28.0-rc.0.linux-amd64 tools/alertmanager
 fi
 
 echo -e "\033[1;34m ============ Alertmanager started ✅ at => http://localhost:9093 ============\033[0m"
@@ -93,13 +93,13 @@ if [ -d "./tools/node_exporter" ]; then
     echo -e "\033[1;34m ============ NodeExporter already downloaded 💯 ============\033[0m"
 else
     echo -e "\033[1;34m ============ NodeExporter downloading ⏳... ============\033[0m"
-    wget -q https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.darwin-amd64.tar.gz
+    wget -q https://github.com/prometheus/node_exporter/releases/download/v1.8.2/node_exporter-1.8.2.linux-amd64.tar.gz
     echo -e "\033[1;34m ============ NodeExporter downloaded 🔰 ============\033[0m"
-    mv node_exporter-1.8.2.darwin-amd64.tar.gz node_exporter.tar.gz
+    mv node_exporter-1.8.2.linux-amd64.tar.gz node_exporter.tar.gz
     tar xfz node_exporter.tar.gz
 
     mv node_exporter.tar.gz archive
-    mv node_exporter-1.8.2.darwin-amd64 tools/node_exporter
+    mv node_exporter-1.8.2.linux-amd64 tools/node_exporter
 fi
 
 echo -e "\033[1;34m ============ NodeExporter started ✅ at => http://localhost:9100/metrics ============\033[0m"
@@ -114,24 +114,24 @@ if [ -d "./tools/grafana" ]; then
     echo -e "\033[1;34m ============ Grafana already downloaded 💯 ============\033[0m"
 else
     echo -e "\033[1;34m ============ Grafana downloading ⏳... ============\033[0m"
-    wget -q https://dl.grafana.com/enterprise/release/grafana-enterprise-11.3.0+security-01.darwin-amd64.tar.gz
+    wget -q https://dl.grafana.com/oss/release/grafana-11.3.0.linux-amd64.tar.gz
     echo -e "\033[1;34m ============ Grafana downloaded 🔰 ============\033[0m"
-    mv grafana-enterprise-11.3.0+security-01.darwin-amd64.tar.gz grafana.tar.gz
+    mv grafana-11.3.0.linux-amd64.tar.gz grafana.tar.gz
     tar xfz grafana.tar.gz
 
     mv grafana.tar.gz archive
-    mv grafana-v11.3.0+security-01 tools/grafana
-    cp config/grafana/datasources.yaml tools/grafana/conf/provisioning/datasources/datasources.yaml
+    mv grafana-v11.3.0 tools/grafana
 fi
 
-GRAFANA_DATA_DIRECTORY="/goinfre/$USER/ft_transcendence/monitoring_system/grafana_data"
-DEFAULTS_GRAFANA_INI="/goinfre/$USER/ft_transcendence/monitoring_system/tools/grafana/conf/defaults.ini"
 
-sed -i '' "s|^http_port *= *3000|http_port = 3030|" $DEFAULTS_GRAFANA_INI
+GRAFANA_DATA_DIRECTORY="grafana_data"
+DEFAULTS_GRAFANA_INI="tools/grafana/conf/defaults.ini"
+
+sed -i "s|^http_port *= *3000|http_port = 3030|" $DEFAULTS_GRAFANA_INI
 echo "Grafana port changed from 3000 to 3030" > logs/grafana_logs
-sed -i '' "s|^data = data|data = $GRAFANA_DATA_DIRECTORY|" $DEFAULTS_GRAFANA_INI
-sed -i '' "s|^logs = data/log|logs = $GRAFANA_DATA_DIRECTORY/log|" $DEFAULTS_GRAFANA_INI
-sed -i '' "s|^path = data/log|path = $GRAFANA_DATA_DIRECTORY/log|" $DEFAULTS_GRAFANA_INI
+sed -i "s|^data = data|data = $GRAFANA_DATA_DIRECTORY|" $DEFAULTS_GRAFANA_INI
+sed -i "s|^logs = data/log|logs = $GRAFANA_DATA_DIRECTORY/log|" $DEFAULTS_GRAFANA_INI
+sed -i "s|^path = data/log|path = $GRAFANA_DATA_DIRECTORY/log|" $DEFAULTS_GRAFANA_INI
 echo "Grafana-Data-Directory changed to $GRAFANA_DATA_DIRECTORY" > logs/grafana_logs
 
 echo -e "\033[1;34m ============ Grafana started ✅ at => http://localhost:3030 ============\033[0m"
